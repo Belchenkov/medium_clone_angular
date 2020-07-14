05a3313b14e185ad9073de91f5fd3986fb96f9db
Aleksey Belchenkov <u608110@gmail.com>
2020-07-14 12:03:04 +0300

    Install NgRx-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthModule } from "./auth/auth.module";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,11 @@ import { AuthModule } from "./auth/auth.module";
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    StoreModule.forRoot({})
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
