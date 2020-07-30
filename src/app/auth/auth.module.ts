e94160a7611eeb921511ef98b9808817f60e3669
Aleksey Belchenkov <u608110@gmail.com>
2020-07-30 04:36:26 +0300

    Save access token to Localstorate

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,6 +10,7 @@ import { reducers } from "./store/reducers";
 import { AuthService } from "./services/auth.service";
 import { RegisterEffect } from "./store/effects/register.effect";
 import { BackendErrorMessagesModule } from "../shared/modules/backendErrorMessages/backendErrorMessages.module";
+import { PersistenceService } from "../shared/services/persistence.service";
 
 const routes: Routes = [
   {
@@ -30,6 +31,9 @@ const routes: Routes = [
     BackendErrorMessagesModule
   ],
   declarations: [RegisterComponent],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    PersistenceService
+  ]
 })
 export class AuthModule {}
--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
 import { of } from "rxjs";
 import { HttpErrorResponse } from "@angular/common/http";
 
@@ -11,6 +12,7 @@ import {
   registerFailureAction,
   registerSuccessAction
 } from "../actions/register.action";
+import { PersistenceService } from "../../../shared/services/persistence.service";
 
 @Injectable()
 export class RegisterEffect {
@@ -19,6 +21,7 @@ export class RegisterEffect {
     switchMap(({request}) => {
       return this.authService.register(request).pipe(
         map((currentUser: CurrentUserInterface) => {
+          this.persistenceService.set('accessToken', currentUser.token);
           return registerSuccessAction({currentUser});
         }),
         catchError((errorResponse: HttpErrorResponse) => {
@@ -28,8 +31,17 @@ export class RegisterEffect {
     })
   ));
 
+  redirectAfterSubmit$ = createEffect(() => this.actions$.pipe(
+    ofType(registerSuccessAction),
+    tap(() => {
+      this.router.navigateByUrl('/');
+    })
+  ), { dispatch: false });
+
   constructor(
     private actions$: Actions,
-    private authService: AuthService
+    private authService: AuthService,
+    private persistenceService: PersistenceService,
+    private router: Router
   ) { }
 }
