446aa774d73edd39a5a962d716372009a1d7b8e3
Aleksey Belchenkov <u608110@gmail.com>
2020-08-09 18:55:55 +0300

    Create Auth Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,8 @@ import { AuthComponent } from './auth/auth.component';
 import { AuthModule } from "./auth/auth.module";
 import { environment } from "../environments/environment";
 import { TopBarModule } from "./shared/modules/topBar/topBar.module";
+import { PersistenceService } from "./shared/services/persistence.service";
+import { AuthInterceptor } from "./shared/services/auth.interceptor.service";
 
 
 @NgModule({
@@ -31,7 +33,14 @@ import { TopBarModule } from "./shared/modules/topBar/topBar.module";
     }),
     TopBarModule
   ],
-  providers: [],
+  providers: [
+    PersistenceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
